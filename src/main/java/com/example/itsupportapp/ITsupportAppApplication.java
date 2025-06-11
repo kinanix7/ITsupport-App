de63b02d45b41fb46d128080e80412b600831e33
Youness elkinani <kinanix7>
2025-06-11 10:15:36 +0100

    first commit

---
+++ src/main/java/com/example/itsupportapp/ITsupportAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.itsupportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITsupportAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ITsupportAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itsupportapp/ITsupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itsupportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITsupportAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
