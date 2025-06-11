55b45db51a679da199108b3d70cb8f8f3cd32059
kawtarmouslim <kawtharmouslim33@gmail.com>
2025-06-11 11:40:57 +0100

    création dto,repository,service

--- src/main/java/com/example/itsupportapp/ITsupportAppApplication.java
+++ src/main/java/com/example/itsupportapp/ITsupportAppApplication.java
@@ -1,7 +1,9 @@
 package com.example.itsupportapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ITsupportAppApplication {
@@ -9,5 +11,10 @@ public class ITsupportAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(ITsupportAppApplication.class, args);
     }
+    @Bean
+    public ModelMapper modelMapper() {
 
+
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/example/itsupportapp/service/EquipementService.java
+++ src/main/java/com/example/itsupportapp/service/EquipementService.java
@@ -1,4 +1,42 @@
 package com.example.itsupportapp.service;
 
+import com.example.itsupportapp.dto.EquipementDto;
+import com.example.itsupportapp.model.Equipement;
+import com.example.itsupportapp.repository.EquipementRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
 public class EquipementService {
+    public  final EquipementRepository equipementRepository;
+    public  final ModelMapper modelMapper;
+    public EquipementDto save(EquipementDto equipementDto) {
+        Equipement equipement=modelMapper.map(equipementDto,Equipement.class);
+        Equipement saved=equipementRepository.save(equipement);
+        return modelMapper.map(saved,EquipementDto.class);
+    }
+    public List<EquipementDto> findAll() {
+        List<Equipement> equipements=equipementRepository.findAll();
+        return equipements.stream()
+                .map(x->modelMapper.map(x,EquipementDto.class))
+                .collect(Collectors.toList());
+    }
+    public EquipementDto update(Long id ,EquipementDto equipementDto) {
+        Equipement equipement=equipementRepository.findById(id).get();
+        equipement.setNom(equipementDto.getNom());
+        equipement.setType(equipementDto.getType());
+        equipement.setEtat(equipementDto.getEtat());
+        equipement.setDateAchat(equipementDto.getDateAchat());
+        Equipement saved=equipementRepository.save(equipement);
+        return modelMapper.map(saved,EquipementDto.class);
+    }
+    public void delete(Long id) {
+        equipementRepository.deleteById(id);
+    }
+
 }
